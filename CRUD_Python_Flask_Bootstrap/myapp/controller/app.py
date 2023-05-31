5e220df89a4f2913437a503846397926e5dd820b
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-05-31 00:37:05 -0500

    Add databases project and update Latex
    Working in more features for latex_pdf.py

---
+++ CRUD_Python_Flask_Bootstrap/myapp/controller/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, url_for, redirect
+import os
+import sys
+sys.path.append('model/')
+from database import Database
+
+class MyApp(Flask):
+    def __init__(self, import_name, template_dir, database):
+        super().__init__(import_name, template_folder=template_dir)
+        self.database = database
+
+    def get_users(self):
+        cursor = self.database.connection.cursor()
+        cursor.execute("SELECT * FROM GUIAS")
+        myresult = cursor.fetchall()
+        cursor.close()
+        column_names = [column[0] for column in cursor.description]
+        insert_object = [dict(zip(column_names, record)) for record in myresult]
+        return insert_object
+
+    def add_user(self, cedula_guia, nombre, apellido, disponibilidad, grupo):
+        if cedula_guia and nombre and apellido and disponibilidad and grupo:
+            cursor = self.database.connection.cursor()
+            sql = "INSERT INTO GUIAS (cedula_guia, nombre, apellido, disponibilidad, grupo) VALUES (%s,%s,%s,%s,%s)"
+            data = (cedula_guia, nombre, apellido, disponibilidad, grupo)
+            cursor.execute(sql, data)
+            self.database.connection.commit()
+
+    def delete_user(self, id):
+        cursor = self.database.connection.cursor()
+        sql = "DELETE FROM GUIAS where id=%s"
+        data = (id,)
+        cursor.execute(sql, data)
+        self.database.connection.commit()
+
+    def edit_user(self, cedula_guia, nombre, apellido, disponibilidad, grupo, id):
+        if cedula_guia and nombre and apellido and disponibilidad and grupo:
+            cursor = self.database.connection.cursor()
+            sql = "UPDATE GUIAS SET cedula_guia = %s, nombre =%s, apellido=%s , disponibilidad=%s, grupo=%s WHERE id = %s"
+            data = (cedula_guia, nombre, apellido, disponibilidad, grupo, id)
+            cursor.execute(sql, data)
+            self.database.connection.commit()
+
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'view', 'templates')
+
+# Crear una instancia de la clase Database
+database = Database(host='localhost', user='root', password='joseph', database='hotel_villa')
+database.connect()
+
+# Crear una instancia de la clase MyApp, pasando la instancia de Database
+app = MyApp(__name__, template_dir, database)
+
+# Rutas 
+@app.route('/')
+def home():
+    data = app.get_users()
+    return render_template('guias_GUI.html', data=data)
+
+# Ruta guardar
+@app.route('/user', methods=['POST'])
+def add_user():
+    cedula_guia = request.form['cedula_guia']
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    disponibilidad = request.form['disponibilidad']
+    grupo = request.form['grupo']
+    app.add_user(cedula_guia, nombre, apellido, disponibilidad, grupo)
+    return redirect(url_for('home'))
+
+# Eliminar
+@app.route('/delete/<string:id>')
+def delete(id):
+    app.delete_user(id)
+    return redirect(url_for('home'))
+
+# Editar
+@app.route('/edit/<string:id>', methods=['POST'])
+def edit(id):
+    cedula_guia = request.form['cedula_guia']
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    disponibilidad = request.form['disponibilidad']
+    grupo = request.form['grupo']
+    app.edit_user(cedula_guia, nombre, apellido, disponibilidad, grupo, id)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
---
+++ CRUD_Python_Flask_Bootstrap/myapp/model/database.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+class Database:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+
+    def execute_query(self, query, params=None):
+        cursor = self.connection.cursor()
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
